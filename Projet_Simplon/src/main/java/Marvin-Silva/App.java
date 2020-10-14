4c7242f266c5830baed7c019e5efb45cb1f1e745
Marvin-Silva <dejesusdasilva.marvin@gmail.com>
2020-10-14 14:59:32 +0200

    firstcommit

---
+++ Projet_Simplon/src/main/java/Marvin-Silva/App.java
@@ -0,0 +1,18 @@
+package Marvin-Silva;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+        System.out.print("Hello World");
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
